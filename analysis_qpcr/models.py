588050b149dce92b640129bcda4d9c11848517c6
Joshua <joshua.llano@upc.edu>
2021-01-04 13:35:55 +0100

    pipeline created

--- analysis_qpcr/models.py
+++ analysis_qpcr/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Sample(models.Model):
-    file = models.FileField(upload_to='files/')
+    file = models.FileField(upload_to='files/raw_files/')
     submit_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
--- analysis_qpcr/views.py
+++ analysis_qpcr/views.py
@@ -3,13 +3,11 @@ from .forms import SampleForm
 
 
 def main_view(request):
-    data_form = None
     if request.method == 'POST':
-        register_form = SampleForm(request.POST, request.FILES)
-        if register_form.is_valid():
-            new_register = register_form
-            new_register.save()
-            data_form = new_register.cleaned_data
+        form = SampleForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'analysis_qpcr/success.html')
     else:
-        register_form = SampleForm()
-    return render(request, 'analysis_qpcr/analysis.html', {'register_form': register_form, 'data_register': data_form})
+        form = SampleForm()
+    return render(request, 'analysis_qpcr/upload_file.html', {'form': form})
