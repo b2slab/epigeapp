64fccdce45b4a982f04c6ecc4915ec9aba92b503
Joshua <joshua.llano@upc.edu>
2021-07-12 15:36:28 +0200

    added workflow para delta Rn + modelo logistico + Random forest. Cambio en el settings.

--- epigen_app/settings.py
+++ epigen_app/settings.py
@@ -138,7 +138,7 @@ ADMINS = (
     ('admin', 'joshua.llano@upc.edu'),
 )
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
