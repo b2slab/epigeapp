af2273d2e8a60ba6f30135db8cbd4aea89caf371
Joshua <joshua.llano@upc.edu>
2021-07-26 12:26:34 +0200

    Notificacion de mail activada en core_app + setting.local comentado mail en terminal + url para debug bar + bug fix typo en requirements/local.txt

--- epigen_app/urls.py
+++ epigen_app/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core_app.urls', namespace='core_app')),
     path('', include('delta_rn.urls', namespace='delta_rn'))
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path(r'__debug__/', include(debug_toolbar.urls)),
+    ]
